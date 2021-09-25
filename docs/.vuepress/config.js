f6ccbee3456005b92aa4bc95633534037c53c1e5
BlaxBerry <chenjiaxu333@gmail.com>
2021-09-25 23:46:21 +0900

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: 'StudyNotes',
+    description: 'Program Study Notes',
+    themeConfig: {
+      smoothScroll: true,
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        {
+          text: 'Links',
+          ariaLabel: 'Links',
+          items: [
+            { text: 'Github', link: 'https://github.com/BlaxBerry' },
+            { text: 'Portfolio', link: 'https://blaxberry.github.io/#/' }
+          ]
+        }
+      ]
+    }
+  }
